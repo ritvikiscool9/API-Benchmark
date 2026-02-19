989ea4d84155de12aa3bbfdf0995cafe9f161dbe
Ritvik Uppal <ritvik.uppal9@gmail.com>
2026-02-18 19:51:05 -0500

    Implementing flags for the url and request amount so they are not hard coded

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"sync"
@@ -14,24 +15,31 @@ type RequestResult struct{
 }
 
 func main() {
-	targetURL := "https://httpbin.org/get"
-	totalRequests := 50
+	urlPtr := flag.String("url", "https://httpbin.org/get", "URL to test")
+	totalRequestsPtr := flag.Int("requests", 10, "number of requests to make")
 
-	fmt.Printf("Starting benchmark on %s with %d requests...\n", targetURL, totalRequests)
+	flag.Parse()
+
+	if *totalRequestsPtr <= 0 {
+		fmt.Printf("Request amount must be greater than 0, defaulting to 10\n")
+		*totalRequestsPtr = 10
+	}
+
+	fmt.Printf("Starting benchmark on %s with %d requests...\n", *urlPtr, *totalRequestsPtr)
 
 	// Define a channel for goroutines to send their results to
-	resultsChan := make(chan RequestResult, totalRequests)
+	resultsChan := make(chan RequestResult, *totalRequestsPtr)
 
 	var wg sync.WaitGroup
 
-	for i := 0; i < totalRequests; i++ {
+	for i := 0; i < *totalRequestsPtr; i++ {
 		wg.Add(1)
 
 		go func() {
 			defer wg.Done()
 			startTime := time.Now()
 			
-			resp, err := http.Get(targetURL)
+			resp, err := http.Get(*urlPtr)
 			if err != nil {
 				// For now, we'll just print errors, but ideally these go to an error channel too
 				fmt.Printf("Error: %s\n", err)
@@ -61,6 +69,6 @@ func main() {
 		totalDuration += result.Duration
 	}
 
-	average := totalDuration / time.Duration(totalRequests)
+	average := totalDuration / time.Duration(*totalRequestsPtr)
 	fmt.Printf("\nTotal Time: %v | Average Request Time: %v\n", totalDuration, average)
 }
