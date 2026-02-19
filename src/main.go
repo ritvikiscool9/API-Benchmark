a624a434f426755cc00f8ff6bc6ca138e67a86c8
Ritvik Uppal <ritvik.uppal9@gmail.com>
2026-02-18 19:21:08 -0500

    Calculating the average time of API calls

--- src/main.go
+++ src/main.go
@@ -7,29 +7,60 @@ import (
 	"time"
 )
 
-func main(){
+// Struct to hod the result of each request
+type RequestResult struct{
+	Status string
+	Duration time.Duration
+}
+
+func main() {
 	targetURL := "https://httpbin.org/get"
 	totalRequests := 50
+
+	fmt.Printf("Starting benchmark on %s with %d requests...\n", targetURL, totalRequests)
+
+	// Define a channel for goroutines to send their results to
+	resultsChan := make(chan RequestResult, totalRequests)
 
 	var wg sync.WaitGroup
 
 	for i := 0; i < totalRequests; i++ {
 		wg.Add(1)
-		go func(){
+
+		go func() {
 			defer wg.Done()
 			startTime := time.Now()
+			
 			resp, err := http.Get(targetURL)
 			if err != nil {
-				fmt.Printf("Request failed: %s\n", err)
-				return // exit this specific goroutine if it failed
+				// For now, we'll just print errors, but ideally these go to an error channel too
+				fmt.Printf("Error: %s\n", err)
+				return
 			}
 			defer resp.Body.Close()
-			timeSince :=time.Since(startTime)
-			fmt.Printf("Status: %s | Time: %v\n", resp.Status, timeSince)
+
+			duration := time.Since(startTime)
+
+			// Send results to the channel
+			resultsChan <- RequestResult{
+				Status:   resp.Status,
+				Duration: duration,
+			}
 		}()
 	}
-	
-	wg.Wait()
-	fmt.Println("Benchmark complete!")
 
+	// Wait for all workers to finish
+	wg.Wait()
+
+	close(resultsChan)
+
+	var totalDuration time.Duration
+	
+	// Loop over the channel 
+	for result := range resultsChan {
+		totalDuration += result.Duration
+	}
+
+	average := totalDuration / time.Duration(totalRequests)
+	fmt.Printf("\nTotal Time: %v | Average Request Time: %v\n", totalDuration, average)
 }
