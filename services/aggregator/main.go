fe622baaa9fa9d01c11ad674776d4c2288b18b9e
Ritvik Uppal <ritvik.uppal9@gmail.com>
2026-04-13 18:20:16 -0400

    Creating a postgres database to store results

--- services/aggregator/main.go
+++ services/aggregator/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"APIbenchmark/proto/pb"
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net"
@@ -11,6 +12,8 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
+
+	_ "github.com/lib/pq"
 )
 
 // gRPC struct
@@ -21,6 +24,7 @@ type server struct{
 	latency int64
 	succesfulRequests int
 	latencies []int64
+	db *sql.DB
 }
 
 func(s *server) SubmitResults(ctx context.Context, req *pb.BatchPayload) (empty *emptypb.Empty, err error){
@@ -61,6 +65,16 @@ func(s *server) SubmitResults(ctx context.Context, req *pb.BatchPayload) (empty 
 		P99Latency = s.latencies[P99Index]
 	}
 
+	// Insert into database
+	insertQuery := `INSERT INTO runs (total_requests, succesful_requests, avg_latency, p95_latency, p99_latency) VALUES ($1, $2, $3, $4, $5)`
+
+	_, err = s.db.Exec(insertQuery, s.totalRequests, s.succesfulRequests, currentAverage, P95Latency, P99Latency)
+	if err != nil {
+		fmt.Printf("Failed to save to database: %v\n", err)
+	} else{
+		fmt.Println("Succesfully saved benchmark to database ")
+	}
+
 	// Log responses
 	fmt.Printf("Received result, total requests: %d. Successful requests: %d. Average Latency: %dms. P95: %dms. P99: %dms\n", 
         s.totalRequests, 
@@ -72,15 +86,44 @@ func(s *server) SubmitResults(ctx context.Context, req *pb.BatchPayload) (empty 
 }
 
 func main(){
+	// Establish database connection
+	connStr := "host=postgres-service port=5432 user=admin password=password123 dbname=benchmarks sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+	
+	// Create table if it doesn't exist 
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS runs (
+		id SERIAL PRIMARY KEY,
+		timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		total_requests INT,
+		succesful_requests INT,
+		avg_latency INT,
+		p95_latency INT,
+		p99_latency INT
+	);`
+
+	_, err = db.Exec(createTableQuery)
+	if err != nil{
+		log.Fatalf("Failed to create table: %v", err)
+	}
+	log.Printf("Database connection established and table verified.")
+
+	
 	// Open gRPC network
 	ln, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("Network connection failed")
 	}
-
+	
 	grpcServer := grpc.NewServer()
 
-	pb.RegisterAggregatorServer(grpcServer, &server{})
+	pb.RegisterAggregatorServer(grpcServer, &server{
+		db: db,
+	})
 
 	log.Printf("Aggregator server listening on port 50051")
 
